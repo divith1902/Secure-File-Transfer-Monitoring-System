a1c34de615896e43dc4a9f4faace7b9f88d4cb6d
divith1902 <vkrishnapatnam23@jnn.edu.in>
2026-02-21 14:39:14 +0530

    Add files via upload

---
+++ hash_utils.py
@@ -0,0 +1,11 @@
+import hashlib
+
+def calculate_hash(file_path):
+    sha256 = hashlib.sha256()
+    try:
+        with open(file_path, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                sha256.update(chunk)
+        return sha256.hexdigest()
+    except Exception as e:
+        return None
